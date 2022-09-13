0004b3281f794c075a5e7625de94e43697f37044
adriantheodor <aatheodor@gmail.com>
2022-09-13 16:08:42 -0400

    added change to assignment two

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h1>
           Welcome to Web Development Fall 2022!
         </h1>
+        <h2>This is the branch for Assignment Two!</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
