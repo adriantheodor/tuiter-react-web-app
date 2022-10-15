7205b55019e264314e6cfac8b104fde60af2e85f
adriantheodor <aatheodor@gmail.com>
2022-10-15 08:30:54 -0400

    a5 blank screen

---
+++ public/tuiter/WhoToFollowList/WhoToFollowListItem.js
@@ -0,0 +1,19 @@
+const WhoToFollowListItem = (who) => {
+
+    `
+                <ul class="list-group">
+                ${
+        who.map(w => `
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col">${w.image}</div>
+                            <div class="col">${w.username}</div>
+                            <div class="col"><button class="btn btn-primary rounded-pill float-end">Follow</button></div>
+                        </div>
+                    </li>
+                    `)
+    }
+                </ul>
+`
+}
+export default WhoToFollowListItem;
---
+++ public/tuiter/WhoToFollowList/index.js
@@ -0,0 +1,10 @@
+import WhoToFollowListItem from "./WhoToFollowListItem.js";
+import who from "./who";
+const WhoToFollowList = () => {
+
+
+        ${WhoToFollowListItem(who)}
+
+
+};
+export default WhoToFollowList;
